642cd7dfabe4b6000d394c0713a8a08e203f4532
rapidofc1 <33822216+rapidofc1@users.noreply.github.com>
2017-12-30 17:01:08 -0700

    Update rc101.py

--- rc101.py
+++ rc101.py
@@ -63,7 +63,7 @@ async def pong(ctx):
 @bot.command()
 @commands.is_owner()
 async def echo(ctx,*, requested : str):
-    await ctx.message.delete(requested)
+    await ctx.message.delete(message)
     await ctx.send(requested)
     
 @bot.command()
